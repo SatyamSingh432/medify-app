5e848cc2aaa0015adb70ab493453ed49072f1bc0
Satyam Singh <satyamrajput7239@gmail.com>
2024-07-19 13:14:06 +0530

    bug

---
+++ src/container/SearchHospital/SearchOptions.jsx
@@ -0,0 +1,36 @@
+import {
+  FaUserMd,
+  FaVials,
+  FaHospital,
+  FaPills,
+  FaAmbulance,
+} from "react-icons/fa";
+import "./SearchOption.css";
+const SearchOptions = () => {
+  <div>
+    <h2 className="section-title">You may be looking for</h2>
+    <div className="categories">
+      <div className="category">
+        <FaUserMd className="category-icon" />
+        <span>Doctors</span>
+      </div>
+      <div className="category">
+        <FaVials className="category-icon" />
+        <span>Labs</span>
+      </div>
+      <div className="category selected">
+        <FaHospital className="category-icon" />
+        <span>Hospitals</span>
+      </div>
+      <div className="category">
+        <FaPills className="category-icon" />
+        <span>Medical Store</span>
+      </div>
+      <div className="category">
+        <FaAmbulance className="category-icon" />
+        <span>Ambulance</span>
+      </div>
+    </div>
+  </div>;
+};
+export default SearchOptions;
